042650f4ef3a662d065d589e0453d797f3e36ebe
vstrog <39245801+vstrog@users.noreply.github.com>
2019-11-27 22:39:33 +0200

    Update data.js

--- src/components/data.js
+++ src/components/data.js
@@ -3,91 +3,91 @@ export const bookData = [
         id: 1,
         title: "Javascript for everyone",
         img: "./img/js.jpg",
-        author: "Jane Doe",
+        author: "Mark Zuckerberg",
         info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum animi nulla rerum ad suscipit minus, dolore mollitia quibusdam ut beatae adipisci porro maiores, nam maxime unde deleniti officiis dolores! Illum?"
     },
     {
         id: 2,
         title: "C programming for everyone",
         img: "./img/c.jpg",
-        author: "Jane Doe",
+        author: "Evan Spiegel",
         info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum animi nulla rerum ad suscipit minus, dolore mollitia quibusdam ut beatae adipisci porro maiores, nam maxime unde deleniti officiis dolores! Illum?"
     },
     {
         id: 3,
         title: "C# for everyone",
         img: "./img/csharp.jpg",
-        author: "Jane Doe",
+        author: "Sir Richard Branson",
         info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum animi nulla rerum ad suscipit minus, dolore mollitia quibusdam ut beatae adipisci porro maiores, nam maxime unde deleniti officiis dolores! Illum?"
     },
     {
         id: 4,
         title: "JAVA on coffe cup for everyone",
         img: "./img/java.jpg",
-        author: "Jane Doe",
+        author: "Carlos Slim",
         info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum animi nulla rerum ad suscipit minus, dolore mollitia quibusdam ut beatae adipisci porro maiores, nam maxime unde deleniti officiis dolores! Illum?"
     },
     {
         id: 5,
         title: "PMP for everyone",
         img: "./img/pmp.jpg",
-        author: "Jane Doe",
+        author: "Larry Page,
         info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum animi nulla rerum ad suscipit minus, dolore mollitia quibusdam ut beatae adipisci porro maiores, nam maxime unde deleniti officiis dolores! Illum?"
     },
     {
         id: 6,
         title: "Ajax for everyone",
         img: "./img/ajax.jpg",
-        author: "Jane Doe",
+        author: "Bill Gates",
         info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum animi nulla rerum ad suscipit minus, dolore mollitia quibusdam ut beatae adipisci porro maiores, nam maxime unde deleniti officiis dolores! Illum?"
     },
     {
         id: 7,
         title: "android for everyone",
         img: "./img/android.jpg",
-        author: "Jane Doe",
+        author: "Elon Musk",
         info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum animi nulla rerum ad suscipit minus, dolore mollitia quibusdam ut beatae adipisci porro maiores, nam maxime unde deleniti officiis dolores! Illum?"
     },
     {
         id: 8,
         title: "design for everyone",
         img: "./img/design.jpg",
-        author: "Jane Doe",
+        author: "John Rockfeller",
         info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum animi nulla rerum ad suscipit minus, dolore mollitia quibusdam ut beatae adipisci porro maiores, nam maxime unde deleniti officiis dolores! Illum?"
     },
     {
         id: 9,
         title: "html for everyone",
         img: "./img/html.jpg",
-        author: "Jane Doe",
+        author: "John Morgan",
         info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum animi nulla rerum ad suscipit minus, dolore mollitia quibusdam ut beatae adipisci porro maiores, nam maxime unde deleniti officiis dolores! Illum?"
     },
     {
         id: 10,
         title: "physics for everyone",
         img: "./img/physics.jpg",
-        author: "Jane Doe",
+        author: "Ray Krock",
         info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum animi nulla rerum ad suscipit minus, dolore mollitia quibusdam ut beatae adipisci porro maiores, nam maxime unde deleniti officiis dolores! Illum?"
     },
     {
         id: 11,
         title: "PYTHON for everyone",
         img: "./img/python.jpg",
-        author: "Jane Doe",
+        author: "Warren Buffett",
         info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum animi nulla rerum ad suscipit minus, dolore mollitia quibusdam ut beatae adipisci porro maiores, nam maxime unde deleniti officiis dolores! Illum?"
     },
     {
         id: 12,
         title: "Ruby for everyone",
         img: "./img/rails.jpg",
-        author: "Jane Doe",
+        author: "Jeff Bezos",
         info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum animi nulla rerum ad suscipit minus, dolore mollitia quibusdam ut beatae adipisci porro maiores, nam maxime unde deleniti officiis dolores! Illum?"
     },
     {
         id: 13,
         title: "SQL DB for everyone",
         img: "./img/sql.jpg",
-        author: "Jane Doe",
+        author: "Meg Whitman",
         info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum animi nulla rerum ad suscipit minus, dolore mollitia quibusdam ut beatae adipisci porro maiores, nam maxime unde deleniti officiis dolores! Illum?"
     },
 
